57b695ab3005005694faa20e7007e84df0a99413
Myles Pritchett <mylesjpritchett@gmail.com>
2024-11-21 11:15:01 +1100

    task setup with create task function

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,16 @@ use crate::prelude::*;
 
 mod error;
 mod prelude;
+mod task;
 mod utils;
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
+    let task = create_task(
+        String::from("Test Task"),
+        120,
+        chrono::NaiveDate::from_ymd_opt(2024, 11, 24).unwrap(),
+    );
+
+    println!("Task: {:?}", task);
     Ok(())
 }
